9f4f53ff632c6a6cca23d5be077b32f6bf28b11e
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-06-27 21:30:40 +0100

    added test for user - failed from applicationcontext not loading

--- server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
+++ server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
@@ -1,10 +1,15 @@
 package com.anime_list.anime_watch_list;
 
+import com.anime_list.anime_watch_list.models.User;
 import com.anime_list.anime_watch_list.repositroies.AnimeRepository;
 import com.anime_list.anime_watch_list.repositroies.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class AnimeWatchListApplicationTests {
@@ -19,5 +24,18 @@ class AnimeWatchListApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+//	------------ User DQ Tests ------------
+
+	@Test
+	public void canFindNameStartingWith(){
+		List<User> found = userRepository.findByNameStartingWith("O");
+		assertThat(found.size()).isEqualTo(2);
+	}
+
+//	Tests failing b/c : failed to load application context
+
+
+//	------------- Anime DQ Tests -----------
 
 }
