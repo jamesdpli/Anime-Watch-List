69540c981fffc0edd84ca0b72a2c35bf454d0032
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-06-27 21:54:20 +0100

    added derived queries for anime

--- server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
+++ server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
@@ -1,5 +1,6 @@
 package com.anime_list.anime_watch_list;
 
+import com.anime_list.anime_watch_list.models.Anime;
 import com.anime_list.anime_watch_list.models.User;
 import com.anime_list.anime_watch_list.repositroies.AnimeRepository;
 import com.anime_list.anime_watch_list.repositroies.UserRepository;
@@ -27,15 +28,32 @@ class AnimeWatchListApplicationTests {
 
 //	------------ User DQ Tests ------------
 
-	@Test
-	public void canFindNameStartingWith(){
-		List<User> found = userRepository.findByNameStartingWith("O");
-		assertThat(found.size()).isEqualTo(2);
-	}
+//	@Test
+//	public void canFindNameStartingWith(){
+//		List<User> found = userRepository.findByNameStartingWith("O");
+//		assertThat(found.size()).isEqualTo(2);
+//	}
 
 //	Tests failing b/c : failed to load application context
 
+//	@Test
+//	public void canFindNameByLetter(){
+//		Integer found = userRepository.countByNameContaining("a");
+//		assertThat(found).isEqualTo(2);
+//	}
+
 
 //	------------- Anime DQ Tests -----------
+
+	@Test
+	public void canIgnoreAnime(){
+		List<Anime> found = animeRepository.findByNameNot("Bleach");
+	}
+
+	@Test
+	public void canFindEpisodeNumber(){
+		List<Anime> found = animeRepository.findByNumberOfEpisodes(75);
+		assertThat(found.size()).isEqualTo(1);
+	}
 
 }
