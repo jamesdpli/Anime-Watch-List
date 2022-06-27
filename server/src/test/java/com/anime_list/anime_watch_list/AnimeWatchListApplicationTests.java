c0e8c319e06d56605859d6523563581167508318
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-27 14:28:40 +0100

    Add spring setup

---
+++ server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anime_list.anime_watch_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeWatchListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
