16234ba91d42804e962be5294be2e9e986fce87c
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-06-28 14:37:20 +0100

    checking the email is correct

--- anime-watch-list-frontend/src/containers/Signup.js
+++ anime-watch-list-frontend/src/containers/Signup.js
@@ -20,6 +20,74 @@ const Signup = ({isLogin, setIsLogin, currentPodCastAcc, setCurrentPodCastAcc}) 
         event.preventDefault();
 
         if (allowedSignup.includes(false)) return;
+
+        const newAnimeUser = {
+            username: inputNewUsername.current.value,
+            password: inputNewPassword.current.value,
+            email: inputNewEmail.current.value,
+            admin: false
+        };
+
+        fetch("http://localhost:8080/users",
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newAnimeUser)
+            })
+            .then(response => response.json())
+            .then(savedAnimeUser => setCurrentAnimeAcc(savedAnimeUser))
+        setIsLogin(!isLogin);
+
+    }
+
+    // Checking if the username already exists
+    const handleExisitedUserName = () => {
+
+        const allUserNames = allUsers.map(user => { return user.username; });
+
+        // initalising a checker with index 0
+        let userNameChecker = [false, allowedSignup[1], allowedSignup[2]];
+
+        if (allUserNames.includes(inputNewUsername.current.value)) {
+            document.querySelector('.new-user-username-input').innerHTML = "This username already exists";
+
+            // didn't pass the username checker
+            setAllowedSignup(userNameChecker);
+
+        }
+        else {
+            document.querySelector('.new-user-username-input').innerHTML = "";
+
+            // pass username checker
+            userNameChecker = [true, allowedSignup[1], allowedSignup[2]];
+            setAllowedSignup(userNameChecker);
+
+        }
+
+    }
+
+    // check if it is the right format of email address
+    const handleCorrectEmail = () => {
+        // initialise a checker, index 1
+        let userEmailChecker = [allowedSignup[0], false, allowedSignup[2]];
+
+        if (!inputNewEmail.current.value.includes("@")) {
+            document.querySelector('.new-user-email-input').innerHTML = "Please put in the correct email"
+
+            // didn't pass the email checker
+            setAllowedSignup(userEmailChecker);
+        }
+        else {
+            document.querySelector('.new-user-email-input').innerHTML = "";
+
+            // pass email checker
+            userEmailChecker = [allowedSignup[0], true, allowedSignup[2]];
+            setAllowedSignup(userEmailChecker);
+
+        }
+    }
+
+    
     return(<></>);
 }
 
