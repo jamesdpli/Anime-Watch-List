fdf3ddb77795006249b5a678ffaeaa9bbac80cb1
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-06-28 21:32:22 +0100

    Add getGenre by id

--- server/src/main/java/com/anime_list/anime_watch_list/controllers/GenreController.java
+++ server/src/main/java/com/anime_list/anime_watch_list/controllers/GenreController.java
@@ -1,15 +1,18 @@
 package com.anime_list.anime_watch_list.controllers;
 
+import com.anime_list.anime_watch_list.models.Anime;
 import com.anime_list.anime_watch_list.models.Genre;
 import com.anime_list.anime_watch_list.repositroies.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -19,9 +22,16 @@ public class GenreController {
     @Autowired
     GenreRepository genreRepository;
 
+    // GET
     @GetMapping
     public ResponseEntity<List<Genre>> getGenres(){
         return new ResponseEntity<>(genreRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}") //localhost:8080/genres/1
+    public ResponseEntity<Optional<Genre>> getGenre(@PathVariable Long id) {
+        Optional<Genre> genre = genreRepository.findById(id);
+        return new ResponseEntity<>(genre, genre.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
     }
 
 }
