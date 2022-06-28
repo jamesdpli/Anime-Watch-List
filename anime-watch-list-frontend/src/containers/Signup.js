0b4067b8b03bfd396719f74c1915536f0931072a
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-06-28 14:33:35 +0100

    added fetch function for users

--- anime-watch-list-frontend/src/containers/Signup.js
+++ anime-watch-list-frontend/src/containers/Signup.js
@@ -1,4 +1,19 @@
-const Signup = () => {
+const Signup = ({isLogin, setIsLogin, currentPodCastAcc, setCurrentPodCastAcc}) => {
+
+    const inputNewEmail = useRef();
+    const inputNewPassword = useRef();
+    const inputNewUsername = useRef();
+    const [allUsers, setAllUsers] = useState();
+    const [passwordShown, setPasswordShown] = useState(false);
+    const [allowedSignup, setAllowedSignup] = useState([false, false, false]);
+
+    console.log(allowedSignup);
+    // to get all the users on the site
+    useEffect(() => {
+        fetch("//http://localhost:8080/users")
+            .then(response => response.json())
+            .then(data => setAllUsers(data))
+    }, [])
     return(<></>);
 }
 
