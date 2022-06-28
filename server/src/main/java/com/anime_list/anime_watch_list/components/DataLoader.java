e55d17e48e97fe5017530a0e8b75022f3e4b1385
sujangurung <sujangurung@sujangurungsmbp.home>
2022-06-28 12:56:36 +0100

    Genre class added

--- server/src/main/java/com/anime_list/anime_watch_list/components/DataLoader.java
+++ server/src/main/java/com/anime_list/anime_watch_list/components/DataLoader.java
@@ -2,7 +2,6 @@ package com.anime_list.anime_watch_list.components;
 
 
 import com.anime_list.anime_watch_list.models.Anime;
-import com.anime_list.anime_watch_list.models.Genre;
 import com.anime_list.anime_watch_list.models.User;
 import com.anime_list.anime_watch_list.models.WatchList;
 import com.anime_list.anime_watch_list.repositroies.AnimeRepository;
@@ -14,9 +13,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -36,19 +33,34 @@ public class DataLoader implements ApplicationRunner {
     @Override
     public void run (ApplicationArguments args) throws Exception {
 
-//      Add Anime
-        Anime anime1 = new Anime("Naruto", LocalDate.of(2002, 10, 03), "Ninja's", Genre.ACTION, 4.6, 500);
-        Anime anime2 = new Anime("Bleach", LocalDate.of(2004, 10, 05), "Soul reaper's", Genre.ACTION, 4.5, 366);
-        Anime anime3 = new Anime("Attack on Titan", LocalDate.of(2013, 04, 07), "Titans destroy humanity", Genre.ACTION, 4.5, 86);
-        Anime anime4 = new Anime("One Piece", LocalDate.of(1999, 10, 03), "Pirates", Genre.ACTION, 4.5, 1014);
-        Anime anime5 = new Anime("Haikyuu", LocalDate.of(2014, 04, 06), "High school volley ball", Genre.SPORTS, 4.5, 73);
-        Anime anime6 = new Anime("Kuroko's Basketball", LocalDate.of(2012, 04, 07), "High school basket ball team", Genre.SPORTS, 4.5, 75);
-        Anime anime7 = new Anime("Bungo Stray Dogs", LocalDate.of(2016, 04, 07), "Detectives ", Genre.ACTION, 4.5, 500);
-        Anime anime8 = new Anime("Death Note", LocalDate.of(2002, 10, 03), "Ninja anime", Genre.ACTION, 4.5, 500);
-        Anime anime9 = new Anime("Slam Dunk", LocalDate.of(2002, 10, 03), "Ninja anime", Genre.ACTION, 4.5, 500);
-        Anime anime10 = new Anime("Jojo's Bizzare Adventure", LocalDate.of(2002, 10, 03), "Ninja anime", Genre.ACTION, 4.5, 500);
+        //      Add Anime
+        Anime anime1 = new Anime("Naruto", LocalDate.of(2002, 10, 03), "Ninja's",  4.6, 500);
+        Anime anime2 = new Anime("Bleach", LocalDate.of(2004, 10, 05), "Soul reaper's", 4.5, 366);
+        Anime anime3 = new Anime("Attack on Titan", LocalDate.of(2013, 04, 07), "Titans destroy humanity",  4.5, 86);
+        Anime anime4 = new Anime("One Piece", LocalDate.of(1999, 10, 03), "Pirates",  4.5, 1014);
+        Anime anime5 = new Anime("Haikyuu", LocalDate.of(2014, 04, 06), "High school volley ball", 4.5, 73);
+        Anime anime6 = new Anime("Kuroko's Basketball", LocalDate.of(2012, 04, 07), "High school basket ball team", 4.5, 75);
+        Anime anime7 = new Anime("Bungo Stray Dogs", LocalDate.of(2016, 04, 07), "Detectives ", 4.5, 500);
+        Anime anime8 = new Anime("Death Note", LocalDate.of(2002, 10, 03), "Ninja anime", 4.5, 500);
+        Anime anime9 = new Anime("Slam Dunk", LocalDate.of(2002, 10, 03), "Ninja anime", 4.5, 500);
+        Anime anime10 = new Anime("Jojo's Bizzare Adventure", LocalDate.of(2002, 10, 03), "Ninja anime", 4.5, 500);
 
-//      Add User
+
+
+
+//        Anime anime1 = new Anime("Naruto", LocalDate.of(2002, 10, 03), "Ninja's", Arrays.asList("Action", "Shounen","Comedy", "SuperPower","Adventure"), 4.6, 500);
+//        Anime anime2 = new Anime("Bleach", LocalDate.of(2004, 10, 05), "Soul reaper's", Arrays.asList("Action", "Shounen","Comedy", "SuperPower"), 4.5, 366);
+//        Anime anime3 = new Anime("Attack on Titan", LocalDate.of(2013, 04, 07), "Titans destroy humanity", Arrays.asList("Action", "Fantasy","Shounen","Comedy", "SuperPower"), 4.5, 86);
+//        Anime anime4 = new Anime("One Piece", LocalDate.of(1999, 10, 03), "Pirates", Arrays.asList( "Fantasy","Shounen","Comedy", "SuperPower","Adventure"), 4.5, 1014);
+//        Anime anime5 = new Anime("Haikyuu", LocalDate.of(2014, 04, 06), "High school volley ball", Arrays.asList( "Fantasy","Shounen","Comedy", "SuperPower","Adventure"), 4.5, 73);
+//        Anime anime6 = new Anime("Kuroko's Basketball", LocalDate.of(2012, 04, 07), "High school basket ball team", Arrays.asList("Fantasy","Shounen","Comedy", "SuperPower","Adventure", "Sports"), 4.5, 75);
+//        Anime anime7 = new Anime("Bungo Stray Dogs", LocalDate.of(2016, 04, 07), "Detectives ", Arrays.asList("Fantasy","Shounen","Comedy", "SuperPower","Adventure"), 4.5, 500);
+//        Anime anime8 = new Anime("Death Note", LocalDate.of(2002, 10, 03), "Ninja anime", Arrays.asList( "Fantasy","Shounen","Comedy", "SuperPower","Adventure", "Horror"), 4.5, 500);
+//        Anime anime9 = new Anime("Slam Dunk", LocalDate.of(2002, 10, 03), "Ninja anime", Arrays.asList( "Fantasy","Shounen","Comedy", "SuperPower","Sports"), 4.5, 500);
+//        Anime anime10 = new Anime("Jojo's Bizzare Adventure", LocalDate.of(2002, 10, 03), "Ninja anime", Arrays.asList( "Fantasy","Comedy", "SuperPower","Adventure"), 4.5, 500);
+
+
+        //      Add User
         User user1 = new User("Onoda", LocalDate.of(1999,9,2), "onodaKun@gmail.com");
         User user2 = new User("John", LocalDate.of(1959,6,25),"lol@gmail.com");
         User user3 = new User("Rem", LocalDate.of(2001,9,4), "Kem@gmail.com");
--- server/src/main/java/com/anime_list/anime_watch_list/models/Genre.java
+++ server/src/main/java/com/anime_list/anime_watch_list/models/Genre.java
@@ -1,19 +1,55 @@
 package com.anime_list.anime_watch_list.models;
 
-public enum Genre {
-    ACTION,
-    ADVENTURE,
-    COMEDY,
-    DRAMA,
-    SLICE_OF_LIFE,
-    FANTASY,
-    HORROR,
-    MYSTERY,
-    PSYCHOLOGICAL,
-    ROMANCE,
-    SCI_FI,
-    SPORTS,
-    SUPERNATURAL,
-    MAGIC
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name ="genres_anime",
+            joinColumns = {@JoinColumn( name ="genres_id")},
+            inverseJoinColumns = {@JoinColumn( name = "animes_id")}
+    )
+    private List<Anime> animes;
+
+    public Genre(String name, List<Anime> animes) {
+        this.name = name;
+        this.animes = animes;
+    }
+
+    public Genre() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Anime> getAnimes() {
+        return animes;
+    }
+
+    public void setAnimes(List<Anime> animes) {
+        this.animes = animes;
+    }
 }
