f08660901b9046a8b9ebbbf435482e4eb17ac516
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-06-28 12:53:55 +0100

    adding routes but commented - error

--- anime-watch-list-frontend/src/App.js
+++ anime-watch-list-frontend/src/App.js
@@ -4,11 +4,29 @@ import animeAll from "./assets/animeAll.mp4";
 // import TextField from "@mui/material/TextField";
 // import { Button } from "@mui/material";
 import zenitsu from "./assets/zenitsu.mp4";
+import './App.css';
+// add usPersistedState
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
+  // const [isLogin, setIsLogin] = usePersistedState('isLogin',false);
+  // const [currentPodCastAcc, setCurrentPodCastAcc] = usePersistedState('currentPodCastAcc', {});
+
   return (
+
+    // <Router>
+    //   <div className="App">
+    //   <Routes>
+    //   <Route path='/login' element={<Login isLogin={isLogin} setIsLogin={setIsLogin}
+    //                                           currentPodCastAcc={currentPodCastAcc}
+    //                                           setCurrentPodCastAcc={setCurrentPodCastAcc}/>}>
+    //       </Route>
+    //     </Routes>
+    //     </div>
+    // </Router>
+
     <div className="main">
-   
+
       <div className="overlay"></div>
       
       <video src={zenitsu} autoPlay loop muted />
