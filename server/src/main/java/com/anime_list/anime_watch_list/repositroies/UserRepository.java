57225f5ad1d0c9b8d99640eacd854318c999f4f8
aisha-png <67974517+aisha-png@users.noreply.github.com>
2022-06-29 16:15:30 +0100

    Added derived queries to user repository and tests passed

--- server/src/main/java/com/anime_list/anime_watch_list/repositroies/UserRepository.java
+++ server/src/main/java/com/anime_list/anime_watch_list/repositroies/UserRepository.java
@@ -3,10 +3,16 @@ package com.anime_list.anime_watch_list.repositroies;
 import com.anime_list.anime_watch_list.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    List<User> findUserByDobAfter(LocalDate dob);
+
+    List<User> findUserByNameStartingWith(String name);
+
+//    List<User> findUserByNameAndNameIgnoreCase(String name);
 
 }
--- server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
+++ server/src/test/java/com/anime_list/anime_watch_list/AnimeWatchListApplicationTests.java
@@ -16,11 +16,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest
 class AnimeWatchListApplicationTests {
 
-//	@Autowired
-//	UserRepository userRepository;
-//
 	@Autowired
 	AnimeRepository animeRepository;
+
+	@Autowired
+	UserRepository userRepository;
+
 
 
 	@Test
@@ -43,8 +44,27 @@ class AnimeWatchListApplicationTests {
 //		assertThat(found).isEqualTo(2);
 //	}
 
-//	ANIME TESTS:
+//	USER TESTS:
 
+	@Test
+	public void canFindUserByDobAfter(){
+		List<User> foundUser = userRepository.findUserByDobAfter(LocalDate.of(1980,01,01));
+		assertThat(foundUser.size()).isEqualTo(9);
+	}
+
+	@Test
+	public void canFindUserByNameStartingWith(){
+		List<User> foundUser = userRepository.findUserByNameStartingWith("J");
+		assertThat(foundUser.size()).isEqualTo(2);
+	}
+
+//	@Test
+//	public void canFindUserByNameAndNameIgnoreCase(){
+//		List<User> foundUser = userRepository.findUserByNameAndNameIgnoreCase("j");
+//		assertThat(foundUser.size()).isEqualTo(1);
+//	}
+
+//	ANIME TESTS:
 	@Test
 	public void canFindAnimeByName(){
 		List<Anime> foundAnime = animeRepository.findAnimeByName("Naruto");
@@ -69,5 +89,22 @@ class AnimeWatchListApplicationTests {
 		List<Anime> foundAnime = animeRepository.findAnimeByNumberOfEpsGreaterThan(50);
 		assertThat(foundAnime.size()).isEqualTo(10);
 	}
+
+//	@Test
+//	public void canFindAnimeByGenreId(){
+//		List<Anime> foundAnime = animeRepository.findAnimeByGenreId(1L);
+//		assertThat(foundAnime.size()).isEqualTo(1);
+//	}
+//	@Test
+//	public void canFindAllByGenres_GenreName(){
+//		List<Anime> foundAllAnimes = animeRepository.findAllByGenres_GenreName(1L);
+//		assertThat(foundAllAnimes.size()).isEqualTo(2);
+//	}
+
+	@Test
+	public void canFindAllByGenres(){
+
+	}
+
 
 }
