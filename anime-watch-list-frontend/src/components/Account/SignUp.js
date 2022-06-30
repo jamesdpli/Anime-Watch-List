b8a90b77dd45b6a1009ee6f6f075a573c0a992ff
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-06-30 22:52:13 +0100

    added basics of sign up page

--- anime-watch-list-frontend/src/components/Account/SignUp.js
+++ anime-watch-list-frontend/src/components/Account/SignUp.js
@@ -20,6 +20,28 @@ const SignUp = () => {
             {/* add more icon buttons */}
           </ul>
         </div>
+        <p className="or-line"><span>Or</span></p>
+        <br/>
+        <p className="sign-up-input-title">Your username</p>
+        <input type="text" className="sign-up-input-box"/>
+        <p className="new-user-username"></p>
+
+        <p className="sign-up-input-title">Your Email</p>
+        <input type="text" className="sign-up-input-box"/>
+        <p className="new-user-email"></p>
+
+        <p className="sign-up-input-title">Password</p>
+        <input type="text" className="sign-up-input-box"/>
+        <p className="new-user-password"></p>
+        <br/>
+
+        <input type="checkbox" id='sign-up-condition-box' className='sign-up-terms-box' />
+        <label htmlFor="sign-up-condition-box" >I agree to terms & conditions.</label>
+        <br />
+
+        <button className='sign-up-btn'>Sign up</button>
+
+        <p>Do you already have an account? <a href="/login">Log in</a></p>
       </form>
     </div>
   );
