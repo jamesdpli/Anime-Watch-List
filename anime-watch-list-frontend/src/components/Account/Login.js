0826d865c1d76d9eff0eec014551d506ea713350
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-07-01 13:49:29 +0100

    added login function

--- anime-watch-list-frontend/src/components/Account/Login.js
+++ anime-watch-list-frontend/src/components/Account/Login.js
@@ -1,11 +1,46 @@
 import "./SignUp.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BsTwitter } from "react-icons/bs";
+import { useRef, useState, useEffect } from "react";
+import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
 // import animeAll from "../FirstPage/animeAll.mp4";
 // import demonSlayer from "../assets/demonSlayer.mp4";
 // import zenitsu from "../FirstPage/zenitsu.mp4";
 
-const Login = () => {
+const Login = ({isLogin, setIsLogin, setCurrentAnimeAcc}) => {
+
+  const navigate = useNavigate();
+  const inputEmail = useRef();
+  const inputPassword = useRef();
+  const [allUsers, setAllUsers] = useState ([]);
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/users")
+    .then(response => response.json())
+    .then(data => setAllUsers(data))
+    },[]);
+
+    const handleLogin = (event) =>{
+      const currentUser = allUsers.filter(user => user.email == inputEmail.current.value &&
+        user.password == inputPassword.current.value);
+        
+        if (currentUser.length == 0) {
+          alert("Invalid email adress or password! Please try again!")
+          return
+        };
+
+        setCurrentAnimeAcc(currentUser[0]);
+        setIsLogin(!isLogin);
+        navigate('/explore');
+        }
+
+        // to show the password or not
+        const handlePasswordShown = (event) => {
+          event.preventDefault();
+          setPasswordShown(!passwordShown);
+      }
+
   return (
     <div className="sign-up-container">
       <form className="sign-up-form">
@@ -19,11 +54,12 @@ const Login = () => {
         <br/> */}
 
         <p className="sign-up-input-title">Your Email</p>
-        <input type="text" className="sign-up-input-box"/>
+        <input type="text" ref={inputEmail} className="sign-up-input-box"/>
         <p className="new-user-email"></p>
 
         <p className="sign-up-input-title">Password</p>
-        <input type="text" className="sign-up-input-box"/>
+        <input type={passwordShown? "text" : "password"} ref={inputPassword} className="sign-up-input-box"/>
+        <button onClick={handlePasswordShown} className="password-shown-button">{passwordShown? <AiOutlineEye className="password-eye"/> : <AiOutlineEyeInvisible className="password-eye"/>}</button>
         <p className="new-user-password"></p>
         <br/>
 
@@ -35,7 +71,7 @@ const Login = () => {
 
         <div>
         <br/>
-        <Link to='/explore'><button className='sign-up-btn'>Log in</button></Link>
+        <button onClick={handleLogin} className='sign-up-btn'>Log in</button>
         </div>
         <p>Do you need an account? <a href="/signup" className="link-a-tag">Create new account</a></p>
       </form>
