eb140355335f7bf2fea025335b0d771efe6384c9
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-07-01 10:59:51 +0100

    Update dataLoader.java

--- server/src/main/java/com/anime_list/anime_watch_list/components/DataLoader.java
+++ server/src/main/java/com/anime_list/anime_watch_list/components/DataLoader.java
@@ -78,9 +78,53 @@ public class DataLoader implements ApplicationRunner {
                         "against the titans and race to discover a way of defeating them before the last " +
                         "walls are breached.",  4.5, 86,
                 "https://media.kitsu.io/anime/poster_images/7442/small.jpg");
-        Anime anime4 = new Anime("One Piece", LocalDate.of(1999, 10, 03), "Pirates",  4.5, 1014);
-        Anime anime5 = new Anime("Haikyuu", LocalDate.of(2014, 04, 06), "High school volley ball", 4.5, 73);
-        Anime anime6 = new Anime("Kuroko's Basketball", LocalDate.of(2012, 04, 07), "High school basket ball team", 4.5, 75);
+        Anime anime4 = new Anime("One Piece", LocalDate.of(1999, 10, 03),
+                "Gol D. Roger was known as the \\\"Pirate King,\\\" the strongest and most infamous being " +
+                        "to have sailed the Grand Line. The capture and death of Roger by the World Government " +
+                        "brought a change throughout the world. His last words before his death revealed the" +
+                        " existence of the greatest treasure in the world, One Piece. It was this revelation that " +
+                        "brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises " +
+                        "an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the " +
+                        "title of the Pirate King.\\nEnter Monkey D. Luffy, a 17-year-old boy who defies your " +
+                        "standard definition of a pirate. Rather than the popular persona of a wicked, hardened, " +
+                        "toothless pirate ransacking villages for fun, Luffy’s reason for being a pirate is one of " +
+                        "pure wonder: the thought of an exciting adventure that leads him to intriguing people and " +
+                        "ultimately, the promised treasure. Following in the footsteps of his childhood hero, Luffy" +
+                        " and his crew travel across the Grand Line, experiencing crazy adventures, unveiling dark " +
+                        "mysteries and battling strong enemies, all in order to reach the most coveted of all " +
+                        "fortunes—One Piece.",  4.5, 1014,
+                "https://media.kitsu.io/anime/poster_images/12/small.jpg");
+        Anime anime5 = new Anime("Haikyuu", LocalDate.of(2014, 04, 06),
+                "Inspired after watching a volleyball ace nicknamed \\\"Little Giant\\\" in action, " +
+                        "small-statured Shouyou Hinata revives the volleyball club at his middle school. The " +
+                        "newly-formed team even makes it to a tournament; however, their first match turns out to be " +
+                        "their last when they are brutally squashed by the \\\"King of the Court,\\\" Tobio " +
+                        "Kageyama. Hinata vows to surpass Kageyama, and so after graduating from middle school, he" +
+                        " joins Karasuno High School's volleyball team—only to find that his sworn rival, Kageyama, " +
+                        "is now his teammate.\\n\\nThanks to his short height, Hinata struggles to find his role on " +
+                        "the team, even with his superior jumping power. Surprisingly, Kageyama has his own problems " +
+                        "that only Hinata can help with, and learning to work together appears to be the only way " +
+                        "for the team to be successful. Based on Haruichi Furudate's popular shounen manga of the" +
+                        " same name, Haikyuu!! is an exhilarating and emotional sports comedy following two " +
+                        "determined athletes as they attempt to patch a heated rivalry in order to make their high" +
+                        " school volleyball team the best in Japan.", 4.5, 73,
+                "https://media.kitsu.io/anime/poster_images/8133/small.jpg");
+        Anime anime6 = new Anime("Kuroko's Basketball", LocalDate.of(2012, 04, 07),
+                "Teikou Junior High School's basketball team is crowned champion three years in a row " +
+                        "thanks to five outstanding players who, with their breathtaking and unique skills, leave " +
+                        "opponents in despair and fans in admiration. However, after graduating, these teammates, " +
+                        "known as \\\"The Generation of Miracles,\\\" go their separate ways and now consider each " +
+                        "other as rivals.\\nAt Seirin High School, two newly recruited freshmen prove that they are " +
+                        "not ordinary basketball players: Taiga Kagami, a promising player returning from the US, " +
+                        "and Tetsuya Kuroko, a seemingly ordinary student whose lack of presence allows him to move " +
+                        "around unnoticed. Although Kuroko is neither athletic nor able to score any points, he was " +
+                        "a member of Teikou's basketball team, where he played as the \\\"Phantom Sixth Man,\\\" who " +
+                        "easily passed the ball and assisted his teammates.Kuroko no Basket follows the journey of" +
+                        "Seirin's players as they attempt to become the best Japanese high school team by winning " +
+                        "the Interhigh Championship. To reach their goal, they have to cross pathways with several " +
+                        "powerful teams, some of which have one of the five players with godlike abilities, whom " +
+                        "Kuroko and Taiga make a pact to defeat.", 4.5, 75,
+                "https://media.kitsu.io/anime/6595/poster_image/small-e15bd563bb282dce553074560247c421.jpeg");
         Anime anime7 = new Anime("Bungo Stray Dogs", LocalDate.of(2016, 04, 07), "Detectives ", 4.5, 500);
         Anime anime8 = new Anime("Death Note", LocalDate.of(2002, 10, 03), "Ninja anime", 4.5, 500);
         Anime anime9 = new Anime("Slam Dunk", LocalDate.of(2002, 10, 03), "Ninja anime", 4.5, 500);
