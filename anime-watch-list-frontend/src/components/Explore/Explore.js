c8ff3fc3f4696c5d4ab45fc7d234a58b344be232
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-07-01 11:13:49 +0100

    linked row and explore

--- anime-watch-list-frontend/src/components/Explore/Explore.js
+++ anime-watch-list-frontend/src/components/Explore/Explore.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import Row from "./Row";
+
+
+const Explore = () => {
+    const [animes, setAnimes] = useState([]);
+
+    useEffect(() => {
+    fetch("https://kitsu.io/api/edge//anime?filter[categories]=adventure", {
+        method: "GET",
+        headers: {
+        Accept: "application/vnd.api+json",
+        "Content-Type": "application/vnd.api+json",
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    }, []);
+
 const Explore = () =>{
     return(
-        <h2>
-            Explore page
-        </h2>
+        <div>
+            <Row animes={animes} />
+            <Row animes={animes} />
+            <Row animes={animes} />
+            <Row animes={animes} />
+            <Row animes={animes} />
+        </div>
     );
 
+    }
 }
 
 export default Explore;
--- anime-watch-list-frontend/src/components/Explore/Row.js
+++ anime-watch-list-frontend/src/components/Explore/Row.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./row.css";
+import "./Row.css";
 
 function Row({ animes }) {
 
