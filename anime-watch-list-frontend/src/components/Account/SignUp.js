c96fdb34fb9cdac4ad9575c92d58133f3dbd7e3c
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-07-01 14:01:22 +0100

    added sign up function

--- anime-watch-list-frontend/src/components/Account/SignUp.js
+++ anime-watch-list-frontend/src/components/Account/SignUp.js
@@ -1,16 +1,64 @@
 // import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import "./SignUp.css";
+import { useRef, useState, useEffect } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 // import React, {useState} from "react";
 // import {BsTwitter} from 'react-icons/bs'; // npm install react-icons
 
 
-const SignUp = () => {
+const SignUp = ({isLogin, setIsLogin, setCurrentAnimeAcc}) => {
+
+    const inputNewEmail = useRef();
+    const inputNewPassword = useRef();
+    const inputNewUsername = useRef();
+    const [allUsers, setAllUsers] = useState();
+    const [passwordShown, setPasswordShown] = useState(false);
+    const [allowedSignup, setAllowedSignup] = useState([false, false, false]);
+
+    // getting all users
+    console.log(allowedSignup);
+    useEffect(() => {
+      fetch("http://localhost:8080/users")
+          .then(response => response.json())
+          .then(data => setAllUsers(data))
+  }, []);
+
+    // POST Mapping for creating a new user --- can change 
+    const handleSignUp = (event) => {
+      event.preventDefault();
+
+      if (allowedSignup.includes(false)) return;
+
+
+      const newAnimeUser = {
+          username: inputNewUsername.current.value,
+          password: inputNewPassword.current.value,
+          email: inputNewEmail.current.value,
+      };
+
+      fetch("http://localhost:8080/users",
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(newAnimeUser)
+            })
+            .then(response => response.json())
+            .then(savedAnimeUser => setCurrentAnimeAcc(savedAnimeUser))
+        setIsLogin(!isLogin);
+      }
+
+      // Add an account and password checker with an action to say if it is wrong
+
+      // To show password or not
+      const handlePasswordShown = (event) => {
+        event.preventDefault();
+        setPasswordShown(!passwordShown);
+      }
+
   return (
-    
     <div className="sign-up-container">
-      
       <form className="sign-up-form">
-        <h1 className="sign-up-title">Sign up with</h1>
+        <h1 className="sign-up-title">Sign Up with</h1>
         {/* <div className="other-sign-up">
           <ul>
             <li className="button-a-tag"><button><BsTwitter/></button></li>
@@ -19,15 +67,16 @@ const SignUp = () => {
         <p className="or-line"><span>Or</span></p>
         <br/> */}
         <p className="sign-up-input-title">Your username</p>
-        <input type="text" className="sign-up-input-box"/>
+        <input type="text" ref={inputNewUsername} className="sign-up-input-box"/>
         <p className="new-user-username"></p>
 
         <p className="sign-up-input-title">Your Email</p>
-        <input type="text" className="sign-up-input-box"/>
+        <input type="text" ref={inputNewEmail} className="sign-up-input-box"/>
         <p className="new-user-email"></p>
 
         <p className="sign-up-input-title">Password</p>
-        <input type="text" className="sign-up-input-box"/>
+        <input type={passwordShown? "text" : "password"} ref={inputNewPassword} className="sign-up-input-box"/><br/>
+        <button onClick={handlePasswordShown} className="password-shown-button">{passwordShown ? <AiOutlineEye className="password-eye" /> : <AiOutlineEyeInvisible className="password-eye" />}</button>
         <p className="new-user-password"></p>
         <br/>
 
