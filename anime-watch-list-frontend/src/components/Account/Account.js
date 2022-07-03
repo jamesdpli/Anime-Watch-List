11edfe51885438911d6ad599558d1c6cadde5524
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-07-03 12:15:30 +0100

    added the users name to the account page

--- anime-watch-list-frontend/src/components/Account/Account.js
+++ anime-watch-list-frontend/src/components/Account/Account.js
@@ -1,18 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import './Account.css';
 
-const Account = ({currentAnimeAcc}) => {
+const Account = ({currentAnimeAcc, setCurrentAnimeAcc}) => {
 
     const navigate = useNavigate();
+
     const handleNavToExplore = () =>{
         navigate('/explore');
     }
+
     return(
         <div className="account-container">
             <div className="welcomeback-title">
                 <ul className="welcome-back-div">
                     <li>Welcome back&nbsp;</li>
-                    <li className="user-name">&nbsp;{currentAnimeAcc.username}&nbsp;</li>
+                    <li className="user-name">&nbsp;{currentAnimeAcc.name}&nbsp;</li>
                 </ul>
             </div>
             
