fb780bffabaecbaae32134ed9f31c57d500d2a8d
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-07-03 11:13:23 +0100

    adding comments

--- anime-watch-list-frontend/src/components/Account/Account.js
+++ anime-watch-list-frontend/src/components/Account/Account.js
@@ -1,16 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "../Footer/Footer";
 
-const Account = () => {
+const Account = ({currentAnimeAcc}) => {
 
     const navigate = useNavigate();
     const handleNavToExplore = () =>{
         navigate('/explore');
     }
     return(
-        <div>
-        <h2>Account Page</h2>
-        {/* <Footer/> */}
+        <div className="account-container">
+            <div className="welcomeback-title">
+                <ul className="welcome-back-div">
+                    <li>Welcome back&nbsp;</li>
+                    <li className="user-name">&nbsp;{currentAnimeAcc.username}&nbsp;</li>
+                </ul>
+            </div>
+            
+
         </div>
     );
 }
