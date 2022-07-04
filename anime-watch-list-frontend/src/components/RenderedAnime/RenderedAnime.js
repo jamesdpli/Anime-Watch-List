2b05dc5a0f1c595e87cc9aeb47408461285b5afb
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-07-04 18:12:18 +0100

    Add RenderedAnime directort

---
+++ anime-watch-list-frontend/src/components/RenderedAnime/RenderedAnime.js
@@ -0,0 +1,68 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import "./renderedAnime.css"
+import Footer from "../../Footer/Footer";
+
+const RenderedAnime = ({ renderedAnime }) => {
+  const params = useParams();
+
+  const [chosenAnime, setchosenAnime] = useState();
+
+  useEffect(() => {
+    if (renderedAnime.length) {
+      let filterChecker = renderedAnime.filter((anime) => {
+        if (anime.name == params.animeName) {
+          return anime;
+        }
+        // console.log(anime)
+      });
+      // console.log(filterChecker)
+      setchosenAnime(filterChecker);
+    }
+  }, [renderedAnime]);
+
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
+
+  return (
+    <>
+      {chosenAnime ? (
+        <>
+          { <div className="container-dynamic">
+            <header
+            className="banner-dynamic">
+            <h1>Hello</h1>
+            <img src={chosenAnime[0].bannerImage} alt={chosenAnime[0].name}/>
+            
+            <div className="banner--fadeButtom" />
+          </header>
+
+          <div className="content-dynamic">
+          <div className="content-left-dynamic">
+            <img src={chosenAnime[0].imageUrl} alt={chosenAnime[0].name}/>
+          </div>
+          <div className="content-right-dynamic">
+            <h1>{chosenAnime[0].name}</h1>
+            <p>{chosenAnime[0].description}</p>
+          </div>
+
+          </div>
+
+
+          <Footer/>
+            </div>
+
+          }
+        </>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default RenderedAnime;
+
