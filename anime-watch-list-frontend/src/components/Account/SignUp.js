275c82505724747dcc589856f1beb820e1632e53
Amber Andrews-Kamara <100682431+aakamara@users.noreply.github.com>
2022-07-05 17:03:54 +0100

    Revert "Styling login and sign up"

--- anime-watch-list-frontend/src/components/Account/SignUp.js
+++ anime-watch-list-frontend/src/components/Account/SignUp.js
@@ -1,10 +1,6 @@
 // import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import "./SignUp.css";
-
-import "./Login.css";
-
 import { useRef, useState } from "react";
-
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { Link } from "react-router-dom";
 // import React, {useState} from "react";
@@ -47,59 +43,55 @@ const SignUp = ({postUser}) => {
       }
 
   return (
+    <div className="sign-up-container">
+      <form onSubmit={handleFormSubmit} className="sign-up-form">
+        <h1 className="sign-up-title">Sign Up with</h1>
 
-    <div>
-      <form className="box-form-1">
-        <div className="left">
-          <div className="overlay1">
-            <h1>Join Us</h1>
-            <h1>today!</h1>
-            <br/>
-            <p>Be part of a big community!</p>
-          </div>
-        </div>
-        
-        <div className="right">
-          <h1>Sign Up here</h1>
-          <br/>
+      <p className="sign-up-input-title">Your name <br/> 
+        <input type="text"  name="name" onChange={handleChange} value={stateUser.name}  className="sign-up-input-box"
+        />
+      </p>
 
-          <p className="sign-up-input-title"></p>
-          <input type="text" placeholder="Name" name="name" onChange={handleChange} value={stateUser.name}  className="sign-up-input-box-1"/>
-        <p className="new-user-username"></p>
-
-        <p className="sign-up-input-title"> 
+        <p className="sign-up-input-title">Enter DOB (YYYY-MM-DD): 
         <br/>
           <input 
-            type="text" 
-            placeholder="Enter DOB (YYYY-MM-DD)" 
+            type="text"  
             name="dob" 
             onChange={handleChange} 
-            className="sign-up-input-box-1" 
+            className="sign-up-input-box" 
             value={stateUser.dob}
           />
         </p>
 
-        <p className="sign-up-input-title"></p>
-        <br/>
-        <input type="text" placeholder="Email" name="email" onChange={handleChange} value={stateUser.email} className="sign-up-input-box-1"/>
-        <p className="new-user-email"></p>
+      <p className="sign-up-input-title">Enter email: 
+        <br/> 
+        <input type="text" placeholder="email" name="email" onChange={handleChange} value={stateUser.email} className="sign-up-input-box"/>
+      </p>
 
-        <p className="sign-up-input-title"></p>
+        <p className="sign-up-input-title">Set password: </p>
+       
+        <input type={passwordShown? "text" : "password"} placeholder="new password" name="password" onChange={handleChange} value={stateUser.password} className="sign-up-input-box"   
+        />
         <br/>
-        <input type={passwordShown? "text" : "password"} placeholder="Password" name="password" onChange={handleChange} value={stateUser.password} className="sign-up-input-box-1"/>
-        <button onClick={handlePasswordShown} className="password-shown-button">{passwordShown ? <AiOutlineEye className="password-eye" /> : <AiOutlineEyeInvisible className="password-eye" />}</button>
+      
+          
+        <button 
+            onClick={handlePasswordShown} 
+            className="password-shown-button">{passwordShown ? <AiOutlineEye className="password-eye" /> : <AiOutlineEyeInvisible className="password-eye" />}</button>
         <p className="new-user-password"></p>
         <br/>
 
-        <button className='sign-up-btn'>Sign up</button><br/><br/>
-        <p>Do you already have an <Link to="/login">account</Link>?</p>
-        </div>
-        
 
-        {/* <input type="checkbox" id='sign-up-condition-box' className='sign-up-terms-box' />
+        <input 
+            type="checkbox" 
+            id='sign-up-condition-box' 
+            className='sign-up-terms-box' />
         <label htmlFor="sign-up-condition-box" >I agree to terms & conditions.</label>
-        <br /> */}
+        <br />
+        <button className='sign-up-btn'>Sign up</button>
 
+        <p>Do you already have an account? <br/>
+       <Link to="/login" className="link-text-tag">Log in</Link></p>
       </form>
     </div>
   );
