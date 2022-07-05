5a436c3d7ea9f63f0b158b72de5e19300c70beb6
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-07-05 10:33:19 +0100

    Remove switch logic, add fetch logic

--- anime-watch-list-frontend/src/components/RenderedAnime/RenderedAnime.js
+++ anime-watch-list-frontend/src/components/RenderedAnime/RenderedAnime.js
@@ -9,7 +9,7 @@ const RenderedAnime = ({ renderedAnime }) => {
 
   const [chosenAnime, setchosenAnime] = useState();
 
-  let animeDescription = "";
+  const [animesDecription, setAnimesDescription] = useState("");
 
   useEffect(() => {
     if (renderedAnime.length) {
@@ -23,76 +23,36 @@ const RenderedAnime = ({ renderedAnime }) => {
     }
   }, [renderedAnime, params.animeName]);
 
-  switch(params.animeName) {
-    case "Naruto":
-      animeDescription = "Moments prior to Naruto Uzumaki's birth, a huge demon known as the Kyuubi, the Nine-Tailed Fox, attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the Kyuubi's rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto.\nNow, Naruto is a hyperactive and knuckle-headed ninja still living in Konohagakure. Shunned because of the Kyuubi inside him, Naruto struggles to find his place in the village, while his burning desire to become the Hokage of Konohagakure leads him not only to some great new friends, but also some deadly foes."
-      break;
-    case "Naruto Shippuden":
-      animeDescription = "It has been two and a half years since Naruto Uzumaki left Konohagakure, the Hidden Leaf Village, for intense training following events which fueled his desire to be stronger. Now Akatsuki, the mysterious organization of elite rogue ninja, is closing in on their grand plan which may threaten the safety of the entire shinobi world.\n\nAlthough Naruto is older and sinister events loom on the horizon, he has changed little in personality—still rambunctious and childish—though he is now far more confident and possesses an even greater determination to protect his friends and home. Come whatever may, Naruto will carry on with the fight for what is important to him, even at the expense of his own body, in the continuation of the saga about the boy who wishes to become Hokage."
-      break;
-    case "Bleach":
-      animeDescription = "Ichigo Kurosaki is an ordinary high schooler—until his family is attacked by a Hollow, a corrupt spirit that seeks to devour human souls. It is then that he meets a Soul Reaper named Rukia Kuchiki, who gets injured while protecting Ichigo's family from the assailant. To save his family, Ichigo accepts Rukia's offer of taking her powers and becomes a Soul Reaper as a result.\nHowever, as Rukia is unable to regain her powers, Ichigo is given the daunting task of hunting down the Hollows that plague their town. However, he is not alone in his fight, as he is later joined by his friends—classmates Orihime Inoue, Yasutora Sado, and Uryuu Ishida—who each have their own unique abilities. As Ichigo and his comrades get used to their new duties and support each other on and off the battlefield, the young Soul Reaper soon learns that the Hollows are not the only real threat to the human world."
-      break;
-    case "Attack on Titan":
-      animeDescription = "Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly terrifying is that their taste for human flesh is not born out of hunger but what appears to be out of pleasure. To ensure their survival, the remnants of humanity began living within defensive barriers, resulting in one hundred years without a single titan encounter. However, that fragile calm is soon shattered when a colossal titan manages to breach the supposedly impregnable outer wall, reigniting the fight for survival against the man-eating abominations.\n\nAfter witnessing a horrific personal loss at the hands of the invading creatures, Eren Yeager dedicates his life to their eradication by enlisting into the Survey Corps, an elite military unit that combats the merciless humanoids outside the protection of the walls. Based on Hajime Isayama's award-winning manga, Shingeki no Kyojin follows Eren, along with his adopted sister Mikasa Ackerman and his childhood friend Armin Arlert, as they join the brutal war against the titans and race to discover a way of defeating them before the last walls are breached."
-      // No clue why IDE does not highlight it orange 😁
-      break;
-    case "One Piece":
-      animeDescription = "Gol D. Roger was known as the \"Pirate King,\" the strongest and most infamous being to have sailed the Grand Line. The capture and death of Roger by the World Government brought a change throughout the world. His last words before his death revealed the existence of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece—which promises an unlimited amount of riches and fame—and quite possibly the pinnacle of glory and the title of the Pirate King.\nEnter Monkey D. Luffy, a 17-year-old boy who defies your standard definition of a pirate. Rather than the popular persona of a wicked, hardened, toothless pirate ransacking villages for fun, Luffy’s reason for being a pirate is one of pure wonder: the thought of an exciting adventure that leads him to intriguing people and ultimately, the promised treasure. Following in the footsteps of his childhood hero, Luffy and his crew travel across the Grand Line, experiencing crazy adventures, unveiling dark mysteries and battling strong enemies, all in order to reach the most coveted of all fortunes—One Piece."
-      break;
-    case "Haikyuu":
-      animeDescription = "Inspired after watching a volleyball ace nicknamed \"Little Giant\" in action, small-statured Shouyou Hinata revives the volleyball club at his middle school. The newly-formed team even makes it to a tournament; however, their first match turns out to be their last when they are brutally squashed by the \"King of the Court,\" Tobio Kageyama. Hinata vows to surpass Kageyama, and so after graduating from middle school, he joins Karasuno High School's volleyball team—only to find that his sworn rival, Kageyama, is now his teammate.\n\nThanks to his short height, Hinata struggles to find his role on the team, even with his superior jumping power. Surprisingly, Kageyama has his own problems that only Hinata can help with, and learning to work together appears to be the only way for the team to be successful. Based on Haruichi Furudate's popular shounen manga of the same name, Haikyuu!! is an exhilarating and emotional sports comedy following two determined athletes as they attempt to patch a heated rivalry in order to make their high school volleyball team the best in Japan."
-      break;
-    case "Kuroko's Basketball":
-      animeDescription = "Teikou Junior High School's basketball team is crowned champion three years in a row thanks to five outstanding players who, with their breathtaking and unique skills, leave opponents in despair and fans in admiration. However, after graduating, these teammates, known as \"The Generation of Miracles,\" go their separate ways and now consider each other as rivals.\nAt Seirin High School, two newly recruited freshmen prove that they are not ordinary basketball players: Taiga Kagami, a promising player returning from the US, and Tetsuya Kuroko, a seemingly ordinary student whose lack of presence allows him to move around unnoticed. Although Kuroko is neither athletic nor able to score any points, he was a member of Teikou's basketball team, where he played as the \"Phantom Sixth Man,\" who easily passed the ball and assisted his teammates.Kuroko no Basket follows the journey of Seirin's players as they attempt to become the best Japanese high school team by winning the Interhigh Championship. To reach their goal, they have to cross pathways with several powerful teams, some of which have one of the five players with godlike abilities, whom Kuroko and Taiga make a pact to defeat."
-      break;
-    case "Bungo Stray Dogs 2":
-      animeDescription = "Guild has entered Yokohoma city. Along with the city, they want Atsushi of The Armed Detective Agency too. As Port Mafia want the city for themselves, a three way war seems unavoidable. While fighting, Atsushi &amp; Akutagawa find help in strange places. And in the background, Dazai continues his endless scheming."
-      break;
-    case "Death Note":
-      animeDescription = "A shinigami, as a god of death, can kill any person—provided they see their victim's face and write their victim's name in a notebook called a Death Note. One day, Ryuk, bored by the shinigami lifestyle and interested in seeing how a human would use a Death Note, drops one into the human realm.\n\nHigh school student and prodigy Light Yagami stumbles upon the Death Note and—since he deplores the state of the world—tests the deadly notebook by writing a criminal's name in it. When the criminal dies immediately following his experiment with the Death Note, Light is greatly surprised and quickly recognizes how devastating the power that has fallen into his hands could be.\n\nWith this divine capability, Light decides to extinguish all criminals in order to build a new world where crime does not exist and people worship him as a god. Police, however, quickly discover that a serial killer is targeting criminals and, consequently, try to apprehend the culprit. To do this, the Japanese investigators count on the assistance of the best detective in the world: a young and eccentric man known only by the name of L."
-      break;
-    case "Slam Dunk":
-      animeDescription = "Hanamichi Sakuragi, infamous for this temper, massive height, and fire-red hair, enrolls in Shohoku High, hoping to finally get a girlfriend and break his record of being rejected 50 consecutive times in middle school. His notoriety precedes him, however, leading to him being avoided by most students. Soon, after certain events, Hanamichi is left with two unwavering thoughts: \"I hate basketball,\" and \"I desperately need a girlfriend.\"\nOne day, a girl named Haruko Akagi approaches him without any knowledge of his troublemaking and asks him if he likes basketball. Hanamichi immediately falls head over heels in love with her, blurting out a fervent affirmative. She then leads him to the gymnasium, where she asks him if he can do a slam dunk. In an attempt to impress Haruko, he makes the leap, but overshoots, instead slamming his head straight into the blackboard. When Haruko informs the basketball team's captain of Hanamichi's near-inhuman physical capabilities, he slowly finds himself drawn into the camaraderie and competition of the sport he had previously held resentment for."
-      break;
-    case "Jojo's Bizzare Adventure 2012":
-      animeDescription = "In 1868, Dario Brando saves the life of an English nobleman, George Joestar. By taking in Dario's son Dio when the boy becomes fatherless, George hopes to repay the debt he owes to his savior. However Dio, unsatisfied with his station in life, aspires to seize the Joestar house for his own. Wielding an Aztec stone mask with supernatural properties, he sets out to destroy George and his son, Jonathan \"JoJo\" Joestar, and triggers a chain of events that will continue to echo through the years to come.\n\nHalf a century later, in New York City, Jonathan's grandson Joseph Joestar discovers the legacy his grandfather left for him. When an archeological dig unearths the truth behind the stone mask, he realizes that he is the only one who can defeat the Pillar Men, mystical beings of immeasurable power who inadvertently began everything.\n\nAdapted from the first two arcs of Hirohiko Araki's outlandish manga series, JoJo no Kimyou na Bouken follows the many thrilling expeditions of JoJo and his descendants. Whether it's facing off with the evil Dio, or combatting the sinister Pillar Men, there's always plenty of bizarre adventures in store."
-      break;
-    case "One Punch man":
-      animeDescription = "The seemingly ordinary and unimpressive Saitama has a rather unique hobby: being a hero. In order to pursue his childhood dream, he trained relentlessly for three years—and lost all of his hair in the process. Now, Saitama is incredibly powerful, so much so that no enemy is able to defeat him in battle. In fact, all it takes to defeat evildoers with just one punch has led to an unexpected problem—he is no longer able to enjoy the thrill of battling and has become quite bored.\n\nThis all changes with the arrival of Genos, a 19-year-old cyborg, who wishes to be Saitama's disciple after seeing what he is capable of. Genos proposes that the two join the Hero Association in order to become certified heroes that will be recognized for their positive contributions to society, and Saitama, shocked that no one knows who he is, quickly agrees. And thus begins the story of One Punch Man, an action-comedy that follows an eccentric individual who longs to fight strong enemies that can hopefully give him the excitement he once felt and just maybe, he'll become popular in the process."
-      break;
-    default:
-      animeDescription = "";
-      break;
-  }
+  useEffect(() => {
+    fetch("https://kitsu.io/api/edge/anime?filter[text]=" + params.animeName)
+    .then(res => res.json())
+    .then(animeDescriptionData => setAnimesDescription(animeDescriptionData.data[0].attributes.description))
+  })
 
   return (
     <>
-      {chosenAnime ? (
+      {
+        chosenAnime ? (
         <>
-          { <div className="container-dynamic">
-            <header
-            className="banner-dynamic">
-            <img src={chosenAnime[0].bannerImage} alt={chosenAnime[0].name}/>
-            {/* <div className="banner--fadeButtom" /> */}
-          </header>
-
+          {
+          <div className="container-dynamic">
+            <header className="banner-dynamic">
+              <img src={chosenAnime[0].bannerImage} alt={chosenAnime[0].name}/>
+              {/* <div className="banner--fadeButtom" /> */}
+            </header>
           <div className="content-dynamic">
-          <div className="content-left-dynamic">
-            <img src={chosenAnime[0].imageUrl} alt={chosenAnime[0].name}/>
-          </div>
-          <div className="content-right-dynamic">
-            <h1>{chosenAnime[0].name}</h1>
-            {/* <p>{chosenAnime[0].description}</p> */}
-            <p>{animeDescription}</p>
-          </div>
-
-          </div>
-
-
-          <Footer/>
+            <div className="content-left-dynamic">
+              <img src={chosenAnime[0].imageUrl} alt={chosenAnime[0].name}/>
             </div>
-
+            <div className="content-right-dynamic">
+              <h1>{chosenAnime[0].name}</h1>
+              {/* <p>{chosenAnime[0].description}</p> */}
+              {/* <p>{animeDescription}</p> */}
+              <p>{animesDecription}</p>
+            </div>
+          </div>
+            <Footer/>
+          </div>
           }
         </>
       ) : (
