b4a3fc7fb0532fece4199f5866b725b6624dbb23
James Dudhi-Price <jamesdudhiprice@gmail.com>
2022-07-07 11:56:46 +0100

    Update Footer

--- anime-watch-list-frontend/src/components/Footer/Footer.js
+++ anime-watch-list-frontend/src/components/Footer/Footer.js
@@ -3,13 +3,15 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footerContent-container">
-        <h3>Copyright Notice</h3>
-        <h3>Privacy Policy Link</h3>
-        <h3>Sitemap</h3>
-        <h3>Logo</h3>
+        <h3>Privacy Policy</h3>
+        {/* <h3>Sitemap</h3> */}
+        {/* <h3>Logo</h3> */}
         <h3>Contact Information</h3>
         <h3>Social Media</h3>
         <h3>Email Sign-up</h3>
+        <h3>Join Now</h3>
+        <h3>Site brought to you by: BNTA</h3>
+        <h3>Copyright Notice ©</h3>
       </div>
     </div>
   );
